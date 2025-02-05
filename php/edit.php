58f06de88b2fe35e92367c33cc873a3493beb906
Cidraque117 <juanfransc@hotmail.com>
2025-02-05 12:57:58 +0100

    primer commit

---
+++ php/edit.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edición del listado de películas</title>
+    <style>
+        .header-container {
+            display: flex;
+            align-items: center;
+            /* Alinea verticalmente al centro */
+            justify-content: space-between;
+            /* Espacio entre el h1 y el botón */
+            gap: 10px;
+            /* Espacio entre el h1 y el botón */
+        }
+
+        .btn {
+            text-decoration: none;
+            padding: 10px 20px;
+            background-color: #007BFF;
+            color: white;
+            border-radius: 5px;
+            display: inline-block;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header-container">
+        <h1>Edición del listado de películas</h1>
+        <a href="index.php" class="btn">VOLVER</a>
+    </div>
+</body>
+
+</html>
